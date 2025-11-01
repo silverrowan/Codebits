23cbaebbf32e5af103ac8d0df36e8491517e2d8e
SilverRowan <silverrowan@gmail.com>
2025-11-01 02:28:34 -0700

    Create jsNumConstructors.js & jsNumMethods.js
    minimal notes added to each at this stage. These structures were mentioned, and will be further referenced/fleshed out later.

---
+++ notesSyntaxEtc/jsNumConstructors.js
@@ -0,0 +1,13 @@
+******JavaScript Number Constructors******
+******************************************
+
+******What is a Constructors******
+
+******How to Use Constructors******
+
+******Common Constructors******
+    Number() 
+        Convert a string into a Number
+        eg. myNumber = Number(stringNumber)
+            (alternatively: if unary +variable can be used to convert to a Number)
+        
